ed64b68dea27c99362f815ff52669ed22bce47b9
ngaukonojane <35097222+ngaukonojane@users.noreply.github.com>
2018-05-31 14:20:18 +0300

    Add files via upload

---
+++ arrowfunction1.js
@@ -0,0 +1,20 @@
+function arrayToList(array) {
+  var list = 0;
+  for (var i = array.length - 1; i >= 0; i--)
+    list = {value: array[i], rest: list};
+  return list;
+}
+
+function nth(list, n) {
+  if (!list)
+    return undefined;
+  else if (n == 0)
+    return list.value;
+  else
+    return nth(list.rest, n - 1);
+}
+
+console.log(nth(arrayToList([100, 40, 20]), 1));
+
+
+const v = (list,n) => (nth(arrayToList([100, 40, 20]), 1));
